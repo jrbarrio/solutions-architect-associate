8359114fef3d1f80229cea4d85fb7bc838bb9652
Jorge Roldán <jorge.roldan@gmail.com>
2019-08-08 20:35:53 +0200

    vpc - configure a rout out from public subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "eu-west-1a"
@@ -30,7 +30,7 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "eu-west-1b"
@@ -39,3 +39,30 @@ resource "aws_subnet" "private" {
     Project = "solutions-architect-associate-vpc"
   }
 }
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Project = "solutions-architect-associate-vpc"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Project = "solutions-architect-associate-vpc"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+

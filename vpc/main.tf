bb4917afed3bdeb189d8fe83a3b98c7d2a4339ed
Jorge Roldán <jorge.roldan@gmail.com>
2019-08-08 20:11:06 +0200

    vpc - create public and private subnets

--- vpc/main.tf
+++ vpc/main.tf
@@ -18,3 +18,24 @@ resource "aws_vpc" "vpc" {
     Project = "solutions-architect-associate-vpc"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Project = "solutions-architect-associate-vpc"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    Project = "solutions-architect-associate-vpc"
+  }
+}

11e9fcf4d9ed9e93a4cd91397184a7fbbd8950ce
Jorge Roldan Barrio <Jorge.RoldanBarrio@zooplus.com>
2019-08-27 09:04:02 +0200

    vpc - create route out through tha NAT Gateway

--- vpc/main.tf
+++ vpc/main.tf
@@ -211,3 +211,9 @@ resource "aws_nat_gateway" "gw" {
   allocation_id = aws_eip.nat_gateway_eip.id
   subnet_id     = aws_subnet.public_subnet.id
 }
+
+resource "aws_route" "r" {
+  route_table_id            = aws_vpc.vpc.default_route_table_id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.gw.id
+}
